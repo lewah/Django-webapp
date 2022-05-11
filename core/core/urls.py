5b93aca6daa7d9603f37cb6c289debf7b1239000
lewah <ingridlewah@gmail.com>
2022-05-11 15:01:49 +0300

    pages app

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+#main url connecting to others urls in apps
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('pages.urls')),
+    # path('gallery/', include('gallery.urls')),
+    # path('blogs/', include('blogs.urls')),
+    # path('products/', include('products.urls')),
+    # path('services/', include('services.urls')),
+    # path('solutions/', include('solutions.urls')),
 ]
+# the urlpatterns help in rendering image in template
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
